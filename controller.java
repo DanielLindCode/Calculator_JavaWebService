c4ea0643106e33b629391faed5ee6c9f220d3e43
undeadnerd <91528618+undeadnerd@users.noreply.github.com>
2022-03-10 14:50:22 +0100

    Add files via upload

--- controller.java
+++ controller.java
@@ -7,31 +7,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class controller {
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/sum")
-	public String calculation(double n1, double n2, String operation) {
 
-		double result;
+	@RequestMapping(method = RequestMethod.GET, value = "/sum")
+	public String calculation(String number1, String number2, String operation) {
+
+		int result;
+
+		int n1 = Integer.parseInt(number1);
+		int n2 = Integer.parseInt(number2);
 
 		switch (operation) {
-		
-		// %2B in URL for "+"
-		case "+":
+
+		case "PLUS":
 			result = n1 + n2;
 			System.out.println(n1 + " + " + n2 + " = " + result);
 			return "<h2>" + n1 + " + " + n2 + " = " + result + "</h2>";
 
-		case "-":
+		case "MINUS":
 			result = n1 - n2;
 			System.out.println(n1 + " - " + n2 + " = " + result);
 			return "<h2>" + n1 + " - " + n2 + " = " + result + "</h2>";
 
-		case "*":
+		case "TIMES":
 			result = n1 * n2;
 			System.out.println(n1 + " * " + n2 + " = " + result);
 			return "<h2>" + n1 + " * " + n2 + " = " + result + "</h2>";
 
-		case "/":
+		case "DIVIDE":
 			result = n1 / n2;
 			System.out.println(n1 + " / " + n2 + " = " + result);
 			return "<h2>" + n1 + " / " + n2 + " = " + result + "</h2>";
@@ -41,4 +43,37 @@ public class controller {
 			return "<h2>Invalid operator!</h2>";
 		}
 	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/sum2")
+	public String calculation2(String input) {
+
+		System.out.println(input);
+		
+		String operators[] = input.split("[0-9]");
+		String operands[] = input.split("[+-/*]");
+		
+		int sum = Integer.parseInt(operands[0]);
+		
+		for (int i = 1; i < operands.length; i++) {
+			if (operators[i].equals("+"))
+			{
+				sum += Integer.parseInt(operands[i]);
+			} else if (operators[i].equals("-"))
+			{
+				sum -= Integer.parseInt(operands[i]);
+			} else if (operators[i].equals("*"))
+			{
+				sum *= Integer.parseInt(operands[i]);
+			} else if (operators[i].equals("/"))
+			{
+				sum /= Integer.parseInt(operands[i]);
+			}
+		}
+		
+		System.out.println(sum);
+		
+		String result = Integer.toString(sum);
+		return "<h2>" + input + " = " + result + "</h2>";
+	}
+
 }
